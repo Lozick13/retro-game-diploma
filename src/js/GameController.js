139ecf94bd2e0b3fb2d57a1706e072e95dd14b4e
Lozick <dlozichnyy2005@gmail.com>
2024-03-31 13:59:13 +0700

    GameController - init()

---
+++ src/js/GameController.js
@@ -0,0 +1,24 @@
+import themes from './themes';
+
+export default class GameController {
+  constructor(gamePlay, stateService) {
+    this.gamePlay = gamePlay;
+    this.stateService = stateService;
+  }
+
+  init() {
+    this.gamePlay.drawUi(themes.desert);
+  }
+
+  onCellClick(index) {
+    // TODO: react to click
+  }
+
+  onCellEnter(index) {
+    // TODO: react to mouse enter
+  }
+
+  onCellLeave(index) {
+    // TODO: react to mouse leave
+  }
+}
